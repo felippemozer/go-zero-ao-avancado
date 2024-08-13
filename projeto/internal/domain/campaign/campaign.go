a630fbfba5399ef3e58c814948b971c0c60e5c6a
felippemozer <felippemozer22@gmail.com>
2024-08-12 23:32:15 -0300

    refactor: include columns on struct correctly

--- projeto/internal/domain/campaign/campaign.go
+++ projeto/internal/domain/campaign/campaign.go
@@ -14,28 +14,29 @@ const (
 )
 
 type Contact struct {
-	Email      string `validate:"email"`
-	CampaignId string
+	ID         string `gorm:"size:50"`
+	Email      string `validate:"email" gorm:"size:100"`
+	CampaignId string `gorm:"size:50"`
 }
 
 type Campaign struct {
-	ID        string    `validate:"required"`
-	Name      string    `validate:"min=5,max=24"`
+	ID        string    `validate:"required" gorm:"size:50"`
+	Name      string    `validate:"min=5,max=24" gorm:"size:100"`
 	CreatedOn time.Time `validate:"required"`
-	Content   string    `validate:"min=5,max=1024"`
+	Content   string    `validate:"min=5,max=1024" gorm:"size:1024"`
 	Contacts  []Contact `validate:"min=1,dive"`
-	Status    string
+	Status    string    `gorm:"size:50"`
 }
 
 func NewCampaign(name string, content string, emails []string) (*Campaign, error) {
-	id := xid.New()
 	contacts := make([]Contact, len(emails))
 	for i, v := range emails {
+		contacts[i].ID = xid.New().String()
 		contacts[i].Email = v
 	}
 
 	campaign := &Campaign{
-		ID:        id.String(),
+		ID:        xid.New().String(),
 		Name:      name,
 		CreatedOn: time.Now(),
 		Content:   content,
