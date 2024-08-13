e56837fe7263fe69952006ecfd085eb44e9bf0e9
felippemozer <felippemozer22@gmail.com>
2024-08-13 08:03:56 -0300

    feat: update get by id response to return amount of emails to send

--- projeto/internal/contract/get_campaign_by_id_dto.go
+++ projeto/internal/contract/get_campaign_by_id_dto.go
@@ -1,8 +1,9 @@
 package contract
 
 type GetCampaignByIdResponse struct {
-	ID      string
-	Name    string
-	Content string
-	Status  string
+	ID                   string
+	Name                 string
+	Content              string
+	Status               string
+	AmountOfEmailsToSend int
 }
